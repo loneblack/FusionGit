af0395417f963ce83b49ccf689f419b1040269a4
lone <loneblack_1@yahoo.com>
2018-07-23 15:12:39 +0800

    Update

--- assignAssetToPerson.php
+++ assignAssetToPerson.php
@@ -170,25 +170,29 @@
 													<tr>
 													  <th></th>
 													  <th>Name</th>
-													  <th>Address</th>
+													  <th>Position</th>
+													  <th>Department</th>
 													</tr>
 													</thead>
 													<tbody>
 													<?php 
-													/*
-															$query = "SELECT SUPPLIERID, SUPPLIERNAME, ADDRESS
-																	  FROM SUPPLIER;"; 
+													
+															$query = "	SELECT e.employeeID,e.name, position, (d.name)as 'department' 
+																		FROM thesis.employee e 
+																		join thesis.department d
+																		on e.DepartmentID = d.DepartmentID;"; 
 																						
 															$result = mysqli_query($dbc, $query);
 															
 															while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 															{
 																echo "<tr>
-																		<td><div style='text-align: center;'><input type='checkbox' class='checkbox' name='suppliers[]' value='{$row['SUPPLIERID']}' style='width: 2rem; height: 2rem;'></div></td>
-																		<td>{$row['SUPPLIERNAME']}</td>
-																		<td>{$row['ADDRESS']}</td>
+																		<td><div style='text-align: center;'><input type='checkbox' class='checkbox' name='suppliers[]' value='{$row['employeeID']}' style='width: 2rem; height: 2rem;'></div></td>
+																		<td>{$row['name']}</td>
+																		<td>{$row['position']}</td>
+																		<td>{$row['department']}</td>
 																	  </tr>";
-															}*/
+															}
 														?>
 													</tbody>
 										
@@ -222,33 +226,50 @@
 												  <table id="example1" class="table table-bordered table-striped">
 													<thead>
 													<tr>
-													  <th>Brand name</th>
-													  <th>Generic name</th>
-													  <th>Dosage</th>
-													  <th>Medicine type</th>
-													  <th>Supplier</th>
+													  <th>Employee Name</th>
+													  <th>Department</th>
+													  <th>Brand</th>
+													  <th>Asset Class</th>
+													  <th>Item Specification</th>
+													  <th>propertyCode</th>
+													  <th>serialNo</th>
+													  <th>macAddress</th>
 													</tr>
 													</thead>
 													<tbody>
 													  <?php
-														$query = "SELECT M.BRANDNAME, M.GENERICNAME, M.DOSAGE, MT.DESCRIPTION, S.SUPPLIERNAME
-																  FROM MEDICINE M JOIN REF_MEDICINETYPE MT
-																					ON M.MEDICINETYPE = MT.MEDICINETYPE
-																				  JOIN MEDICAL_SUPPLIERS MS
-																					ON M.MEDICINEID = MS.MEDICINEID
-																				  JOIN SUPPLIER S
-																					ON MS.SUPPLIERID = S.SUPPLIERID;";
+														$query = "SELECT (e.name)AS 'EmployeeName',	(d.name)AS 'Department',
+																(b.name)AS 'Brand', (ac.name)AS 'AssetClass', at.itemSpecification,
+																a.propertyCode, a.serialNo, a.macAddress 
+																	FROM thesis.assetassignment aa
+																	join personresponsible pr
+														            on aa.personresponsible_id = pr.id
+														            join employee e
+														            on pr.employeeID = e.employeeID
+														            join asset a
+														            on a.assetID = aa.assetID
+														            join assettype at
+														            on a.assetTypeID = at.assetTypeID
+														            join ref_assetclass ac
+														            on at.assetClass = ac.assetClassID
+														            join ref_brand b
+														            on at.brand = b.brandID
+														            join department d
+														            on d.DepartmentID = e.DepartmentID;";
 																						 
 														$result = mysqli_query($dbc, $query);
 														
 														while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 														{
 															echo "<tr>
-																	<td>{$row['BRANDNAME']}</td>
-																	<td>{$row['GENERICNAME']}</td>
-																	<td>{$row['DOSAGE']}</td>
-																	<td>{$row['DESCRIPTION']}</td>
-																	<td>{$row['SUPPLIERNAME']}</td>
+																	<td>{$row['EmployeeName']}</td>
+																	<td>{$row['Department']}</td>
+																	<td>{$row['Brand']}</td>
+																	<td>{$row['AssetClass']}</td>
+																	<td>{$row['itemSpecification']}</td>
+																	<td>{$row['propertyCode']}</td>
+																	<td>{$row['serialNo']}</td>
+																	<td>{$row['macAddress']}</td>
 																  </tr>";
 														}
 													  ?>
