6aba87cf0a01c4e212004a6782e78f22bd9e7544
sia661 <41309363+sia661@users.noreply.github.com>
2018-07-24 15:05:21 +0800

    Add files via upload

--- addAssetClass.php
+++ addAssetClass.php
@@ -8,6 +8,12 @@
 		<div>
 			<label>Name</label>
 			<input type="text" name="name" placeholder="Name" required>
+			
+			<label>Brand</label>
+			<input type="text" name="name" placeholder="Brand" required>
+			
+			<label>Item Specifications</label>
+			<input type="text" name="name" placeholder="Item Specifications" required>
 		</div>
 		<button type="submit">Submit</button>
 	</form>
