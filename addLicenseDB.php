de91d30f82be5b5c49ca3f1d8193ab9690181955
CoolKid0208 <marcglennable@gmail.com>
2018-08-03 02:20:55 +0800

    Di ko siya mainsert sa db

---
+++ addLicenseDB.php
@@ -0,0 +1,16 @@
+<?php
+	require_once("mysqlconnect.php");
+	
+	$AssetClass = $_POST['assetclass'];
+	$dateAcquired = $_POST['dateAcquired'];
+	$dateExpired = $_POST['dateExpired'];
+	
+	
+	$sql = "INSERT INTO license ( AssetID, dateAquired, dateExpired) VALUES ('{$AssetClass}', '{$dateAcquired}', '{$dateExpired}')";
+
+	echo $sql;
+
+	$result = mysqli_query($dbc, $sql);
+
+	
+?>
