11278b4e004777a349e454b6c0ab35857bb4c297
CoolKid0208 <marcglennable@gmail.com>
2018-08-12 18:08:07 +0800

    Reloading back to same page pero walang message(pls edit)

--- addLicenseDB.php
+++ addLicenseDB.php
@@ -1,14 +1,21 @@
 <?php
+	
 	require_once("mysqlconnect.php");
 	
 	$assetID = $_POST['assetID'];
 	$dateAcquired = $_POST['dateAcquired'];
 	$dateExpired = $_POST['dateExpired'];
+	$message = "License successfully added ";
 	
 	
 	$sql = "INSERT INTO license (assetID, dateAcquired, dateExpired) VALUES ('{$assetID}', '{$dateAcquired}', '{$dateExpired}');";
 
 	$result = mysqli_query($dbc, $sql);
+	echo "License successfully added ";
+	
+	header("Location: addLicense.php"); 
+	
+
 
 	
 ?>
