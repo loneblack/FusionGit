866017d1688f212a09a15fb0dfc45256eac655ae
CoolKid0208 <marcglennable@gmail.com>
2018-08-18 16:48:38 +0800

    new

---
+++ AllAssetTable.php
@@ -0,0 +1,185 @@
+<?php
+session_start();
+require_once("mysqlconnect.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Fusion IT Asset Management System</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="layout/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="layout/font-awesome.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="layout/dataTables.bootstrap.min.css">
+  <link href="layout/AssetsCssBootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="layout/AssetsCssFont-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="layout/AssetsCssStyle.css" rel="stylesheet" />
+
+</head>
+
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <strong>Email: </strong>fushion@dlsu.edu.com
+                    &nbsp;&nbsp;
+                    <strong>Support: </strong>+90-897-678-44
+					&nbsp;&nbsp;
+					<strong>Logout </strong>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+
+                    <img src="resource/logo.png" />
+                </a>
+            </div>
+
+            <div class="left-div">
+                <div class="user-settings-wrapper">
+                    <ul class="nav">
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a class="menu-top-active" href="employees-home.html">Dashboard</a></li>
+                            <li><a href="#">Adding</a>
+								<ul>
+								<li> <a href="addAsset.php">Add Asset</a> </li>
+								<li> <a href="addDesktop.php">Add Desktop</a> </li>
+								<li> <a href="addEmployee.php">Add Employee</a> </li>
+								<li> <a href="addLicense.php">Add License</a> </li>
+								<li> <a href="addSoftware.php">Add Software</a> </li>
+								<li> <a href="addWarranty.php">Add Warranty</a> </li>
+								</ul>
+							</li>
+							<li><a href="#">Assigning</a>
+								<ul>
+								<li> <a href="assignRoomToDepartment.php">Assign Room</a> </li>
+								<li> <a href="assignAssetToPerson.php">Assign Asset</a> </li>
+								</ul>
+							</li>
+                            <li><a href="#">Data Tables</a>
+								<ul>
+								<li> <a href="assetTesting.php">Asset Testing Table</a> </li>
+								</ul>
+							</li>
+                            <li><a href="#">Forms</a></li>
+                             <li><a href="login.html">Login Page</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Main content -->
+	<body>	  
+    <section class="content">
+      <div class="row">
+	    <div class="col-sm-5">
+		  <div class="box box-primary" style="width : 1000px; padding-left: 100px" align="center">
+			<div class="box-header">
+			  <h3 class="box-title">For Disposal</h3>
+			</div>
+			<!-- /.box-header -->
+			<div class="box-body">
+						  <table id="example2" class="table table-bordered table-striped">
+							<thead>
+							<tr>
+							  <th>Status</th>
+							  <th>Property Code</th>
+							  <th>Serial Number</th>
+							  <th>MAC Address</th>
+							  <th>Brand</th>
+							  <th>Asset Type</th>
+							  <th>Item Specification</th>
+							</tr>
+							</thead>
+							<tbody>
+								<?php
+									$query = "SELECT s.description,assetID, a.assetTypeID, propertyCode, serialNo, macAddress, itemSpecification, (b.name)as 'brand',(ac.name) as 'assetclass' FROM thesis.asset a
+												JOIN ref_status s
+												  ON a.status = s.statusID
+												join assettype at on a.assetTypeID = at.assetTypeID
+												join ref_brand b on at.brand = b.brandId 
+												join ref_assetclass ac on at.assetClass = ac.assetClassID;";
+																
+									$result = mysqli_query($dbc, $query);
+									
+									while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+									{
+										echo "<tr>
+												<td>{$row['description']}</td>
+												<td>{$row['propertyCode']}</td>
+												<td>{$row['serialNo']}</td>
+												<td>{$row['macAddress']}</td>
+												<td>{$row['brand']}</td>
+												<td>{$row['assetclass']}</td>
+												<td>{$row['itemSpecification']}</td>
+											  </tr>";
+									}
+								?>
+							</tbody>
+						
+						  </table>
+						</div>
+		  </div>
+		  <!-- /.box -->
+						</div>
+					<!-- /.col -->
+					
+      
+	  
+
+<!-- jQuery 3 -->
+<script src="layout/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="layout/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="layout/jquery.dataTables.min.js"></script>
+<script src="layout/dataTables.bootstrap.min.js"></script>
+
+<script>
+  $(function () {
+    $('#example1').DataTable()
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': true,
+      'searching'   : true,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : true
+    })
+  })
+</script>
+</body>
+</html>
